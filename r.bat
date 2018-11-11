414c0d1c8732a38a6b6ef0d240a3ad48db0a7079
Daniel <mytasmic@gmail.com>
2018-11-12 00:40:42 +0300

    updated requests

--- r.bat
+++
@@ -1,8 +0,0 @@
-git pull
-git filter-branch --force --index-filter "git rm --cached --ignore-unmatch horopter/alembic.ini" --prune-empty --tag-name-filter cat -- --all
-git push
-git filter-branch --force --index-filter "git rm --cached --ignore-unmatch horopter\alembic.ini" --prune-empty --tag-name-filter cat -- --all
-git push
-git filter-branch --force --index-filter "git rm --cached --ignore-unmatch alembic.ini" --prune-empty --tag-name-filter cat -- --all
-git push
-PAUSE
