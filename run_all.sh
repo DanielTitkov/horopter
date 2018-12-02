790824f2f6d89dbed6a8b6dc0354f6489cf1db44
Daniel <mytasmic@gmail.com>
2018-12-02 23:18:27 +0300

    run all sh

--- run_all.sh
+++ run_all.sh
@@ -8,6 +8,7 @@ source /home/ubuntu/horopter/venv/bin/activate
 python -m horopter.load_articles
 python -m horopter.analyze_articles
 python -m horopter.aggregate
+python -m horopter.drop_old_records
 # python -m horopter.drop_old_records
 # /home/ubuntu/horopter/horopter/analyze_articles.py
 # ls | xargs -I {} echo "$(pwd -P)/{}" | xargs | sed 's/ /","/g'
